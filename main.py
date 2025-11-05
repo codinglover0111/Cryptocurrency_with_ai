d4f60c1786a142e638585bad0162fb625e5c86ab
jungminjo <cjm01115@naver.com>
2025-11-05 14:18:00 +0900

    콜드 스타크

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 import logging
 import time
 from datetime import datetime
+import os
 
 import pytz
 import schedule
@@ -53,6 +54,10 @@ def run_scheduler() -> None:
     schedule.every().hour.at(":43").do(job)
     # 5분 주기로 실행
     schedule.every(5).minutes.do(review_job)
+    
+    # if os.getenv("COLD_START") == "1":
+    job()
+    review_job()
 
     while True:
         try:
